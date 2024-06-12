0156a38615374794395f0f54b6474fb09ea5ce5b
Vikas <vikas.mishra@qure.ai>
2024-06-12 21:45:25 +0530

    Added user profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSets, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
